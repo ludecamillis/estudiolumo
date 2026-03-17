5cc9a9b809d31cf8afa8c04969689bedf295642c
ludecamillis <36630661+ludecamillis@users.noreply.github.com>
2026-03-17 11:08:02 -0300

    Create select.tsx

---
+++ components/ui/select.tsx
@@ -0,0 +1,27 @@
+export function Select({ children }: any) {
+  return <select className="border p-2 w-full">{children}</select>
+}
+
+export function SelectContent({ children }: any) {
+  return <>{children}</>
+}
+
+export function SelectGroup({ children }: any) {
+  return <>{children}</>
+}
+
+export function SelectItem({ children, value }: any) {
+  return <option value={value}>{children}</option>
+}
+
+export function SelectLabel({ children }: any) {
+  return <>{children}</>
+}
+
+export function SelectTrigger({ children }: any) {
+  return <>{children}</>
+}
+
+export function SelectValue() {
+  return null
+}
